d2c6744ba4bacddd2b64e8f98ffa3f39bb3e804b
mischkiey <mischkieycf@gmail.com>
2020-08-29 23:19:31 -0400

    Setting up for deployment

---
+++ build/precache-manifest.1a38b5edcba6c5d2855027749347caf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf4eb17f9779af54969da81429b2852",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68e340c0998c841b5470",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "fee97dcac6879a76117f",
+    "url": "/static/js/2.c941505c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.c941505c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68e340c0998c841b5470",
+    "url": "/static/js/main.fbbe8893.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  }
+]);
